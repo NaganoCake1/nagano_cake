266af598d7f0964a6b714af93f8b5d8316977f76
Your Name <100946724+ryodmm@users.noreply.github.com>
2022-06-14 14:33:56 +0000

    [add_genre_edit]nagano_cake

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :genres
+  end
+
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
