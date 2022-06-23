bb0ea04b5ae426ffb4674da2e926796547e4c1d5
Your Name <100946724+ryodmm@users.noreply.github.com>
2022-06-23 05:21:44 +0000

    [add_items]nagano_cake

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -32,7 +32,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :price, :genre_id, :genre_name, :is_active)
+    params.require(:item).permit(:image, :name, :introduction, :price, :genre_id, :is_active)
   end
 
 end
